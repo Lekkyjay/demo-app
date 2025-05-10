26734721ac22e824315e67350248512686b2a84d
‘Lekky <lekkyjay@yahoo.com>
2025-05-10 23:57:23 +0200

    Dockerized frontEnd and backEnd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    host: true, // Allow external connections when dev server is running inside docker container
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Proxy API requests to Express server. API_URL env variable not needed (in development)
+        target: 'http://backend:5000', // Proxy API requests to backend service. VITE_API_URL variable not needed (in development)
         changeOrigin: true,
       },
     },
